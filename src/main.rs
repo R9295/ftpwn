70c84878533d2c5d017e752aa8e0cfb3f4548261
yoavweber <yoav.weber@code.berlin>
2022-05-16 22:23:04 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,15 @@ use clap::Parser;
 pub use server::Server;
 pub use server::Connect_To_Server;
 
-use itertools::Itertools;
 use std::fs::File;
 use std::io::BufReader;
 use std::thread;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::{
-    fs::read_to_string,
     io::{ Result},
     path::Path,
     sync::{Arc, Condvar, Mutex},
     time::Instant,
-    vec::Vec,
 };
 use threadpool::ThreadPool;
 
@@ -49,18 +46,10 @@ fn line_producer(file_name: &str,sender: &Sender<String>)->  std::io::Result<()>
     let f = File::open(&file_name)?;
     let reader = BufReader::new(f);
     let mut line_iter = reader.lines().map(|l| l.unwrap());
-    let mut buffer:Vec<String> =  Vec::new();
     loop {
         match line_iter.next() {
             Some(line) => {
                 sender.send(line);
-                // if buffer.len() == 10 {
-                //     sender.send(buffer);
-                //     buffer = Vec::new();
-                //     buffer.push(line);
-                // } else {
-                //     buffer.push(line);
-                // }
             },
             None => { break }
         }
@@ -76,34 +65,14 @@ fn main() -> Result<()> {
     let args1 = RawArgs::parse();
 
     let host = args1.host;
-    // let chunks = args.credentials;
-    let (sender1, receiver1): (Sender<String>, Receiver<String>) = channel();
-    // let (sender1, receiver1): (Sender<Vec<String>>, Receiver<Vec<String>>) = channel();
-    thread::spawn(move || line_producer(&args1.credentials,&sender1));
-    // let mut debug_vector:Vec<String> =  Vec::new();
-    // debug_vector.push(String::from("admin:123456"));
-    // debug_vector.push(String::from("admin:test"));
-    // debug_vector.push(String::from("admin:t"));
-    // debug_vector.push(String::from("admin:sadasd"));
-    // let mut test:Vec<Vec<String>> = Vec::new();
-    // let new_vec = debug_vector.clone();
-    // test.push(debug_vector);
-    // test.push(new_vec);
+    let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
+    thread::spawn(move || line_producer(&args1.credentials,&sender));
 
     let now = Instant::now();
     let pool = ThreadPool::new(4);
     let manage_threads = HandleThreads::new();
     
-    // think about abstracting this part, mabe in the manage_threads object?
-    let (sender, receiver): (Sender<u32>, Receiver<u32>) = channel();
-    // for cred_chunk in test {
-    //     let host = host.clone();
-    //     let sender = sender.clone();
-    //     let manage_threads = manage_threads.clone();
-    //     println!("!!");
-    //     pool.execute(move || pwn_server(&manage_threads,host,sender,cred_chunk));
-    // }
-    for line in receiver1 {
+    for line in receiver {
         let host = host.clone();
         let sender = sender.clone();
         let manage_threads_clone = manage_threads.clone();
@@ -151,7 +120,6 @@ impl ManageThreads for HandleThreads {
         let &(ref lock, ref cvar) = &*self.pair.clone();
         let mut done = lock.lock().unwrap();
         *done = true;
-        // We notify the condvar that the value has changed.
         cvar.notify_one();
         return true
     }
@@ -159,6 +127,7 @@ impl ManageThreads for HandleThreads {
 fn pwn_server(manage_threads:&HandleThreads,host: String,sender: Sender<u32>,cred_chunk: String) -> () {
 // fn pwn_server(manage_threads:&HandleThreads,host: String,sender: Sender<u32>,cred_chunk: Vec<String>) -> () {
     println!("entering server");
+
     let server = Server{host:&host};
     let mut stream = server.connect_tcp();
     if server.can_connect(&mut stream) {
@@ -169,7 +138,6 @@ fn pwn_server(manage_threads:&HandleThreads,host: String,sender: Sender<u32>,cre
                     if code == 1 {
                         println!("------------- SUCCESS -------------");
                         println!("{}", cred_chunk);
-                        // println!("{}", cred);
                         println!("------------- SUCCESS -------------");
                         manage_threads.finish_work();
                     }
